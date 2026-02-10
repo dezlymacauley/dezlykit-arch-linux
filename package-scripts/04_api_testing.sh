9c42c191aa9d37c6804a1a9a4126a2fa5b8190c0
dezlymacauley <dezlymacauley@proton.me>
2026-02-10 13:38:43 +0200

    Added httpie for api testing and bash language support

---
+++ package-scripts/04_api_testing.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# A cli tool written in Python, for API testing
+sudo pacman -S --needed httpie
---
+++ package-scripts/06_shell_scripting.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Language support for `.sh` files
+sudo pacman -S --needed bash-language-server
