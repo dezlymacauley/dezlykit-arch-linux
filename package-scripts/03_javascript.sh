eeecc2ae1582cf7d83b9e11a299c5a1f84e87489
dezlymacauley <dezlymacauley@proton.me>
2026-02-10 09:16:23 +0200

    Added npm global installs, and typescript

--- package-scripts/03_javascript.sh
+++ package-scripts/03_javascript.sh
@@ -6,3 +6,32 @@ sudo pacman -S --needed nodejs
 # The most widely used JavaScript package manager
 # On Arch Linux, the `nodejs` package does not include `npm`
 sudo pacman -S --needed npm
+
+# Create a custom directory for npm packages that are installed 
+# with the command `npm install -g name-of-package`
+mkdir -p "${HOME}/.npm-global-pkgs"
+
+# Set npm to use this directory 
+# npm packages installed with the command `npm install -g name-of-package`,
+# will installed to this location.
+npm config set prefix "${HOME}/.npm-global-pkgs"
+
+echo
+echo "Packages installed with 'npm install -g' will be saved at:"
+echo "$(npm prefix -g)"
+echo
+
+# A superset of JavaScript language, 
+# that has additional syntax for types and additional features.
+npm install -g typescript
+
+# Language support for TypeScript
+npm install -g typescript-language-server
+
+echo
+echo "The following npm packages have been installed:"
+echo
+
+echo
+npm list -g
+echo
---
+++ saved-configs/.zprofile
@@ -0,0 +1,4 @@
+# This allows the shell to have access to the binaries of npm
+# packages that were installed with the command:
+# `npm install -g name-of-package`
+export PATH="$HOME/.npm-global-pkgs/bin:$PATH"
