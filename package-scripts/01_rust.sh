89698cd7883c4955780fae947fa01889c6c92007
dezlymacauley <dezlymacauley@proton.me>
2026-02-11 07:46:07 +0200

    Added mise

--- package-scripts/01_rust.sh
+++ package-scripts/01_rust.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+#______________________________________________________________________________
+
+# SECTION: Rustup
 
 # The Rust toolchain manager. 
 sudo pacman -S --needed rustup
@@ -12,3 +16,53 @@ rustup default stable
 # This will add `rust-analyzer` to the current Rust toolchain 
 # of the directory where this command is executed
 rustup component add --toolchain stable rust-analyzer
+
+#______________________________________________________________________________
+
+# SECTION: Cargo-binstall
+
+# `cargo-binstall` is a drop-in replacement for the `cargo install` command.
+# Instead of using the command `cargo install`, 
+# you can use the command `cargo binstall`
+
+# This will look for a pre-compiled binary of a package and download it.
+# If there is no pre-compiled binary, 
+# then it will fall back to using the `cargo install` command,
+# which will try to build the package from source.
+
+# The catch is that to install `cargo-binstall` you need to build 
+# it from source once (which can take about 6 minutes).
+
+# This will add an executable binary to `$HOME/.cargo/bin/`
+if ! command -v cargo-binstall 1>/dev/null 2>/dev/null; then
+    printf '\n'
+    cargo install cargo-binstall
+else
+    # Once `cargo-binstall` is installed, you can update it by 
+    # using `cargo binstall` to download a pre-compiled binary, 
+    # rather than having to build it from source again.
+    printf '\n'
+    cargo binstall --no-confirm cargo-binstall
+fi
+
+#______________________________________________________________________________
+
+# SECTION: Cargo-module
+
+# A cargo plugin for visualizing/analyzing a crate's internal structure.
+
+printf '\n'
+cargo binstall --no-confirm cargo-modules
+printf '\n'
+
+#______________________________________________________________________________
+
+# SECTION: Setup mise
+
+# A polyglot tool version manager written in Rust
+
+printf '\n'
+cargo binstall --no-confirm mise
+printf '\n'
+
+#______________________________________________________________________________
